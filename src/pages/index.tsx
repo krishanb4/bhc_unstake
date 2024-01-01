ac49bad6be09f02d60ae6fa9fde525fcb6caa430
Krishan Bandara <34151165+krishanb4@users.noreply.github.com>
2024-01-02 02:07:39 +0530

    Update meta description in index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
     <>
       <Head>
         <title>BillionHappiness Unstake</title>
-        <meta name="description" content="IgnoreFud Dashboard" />
+        <meta name="description" content="BillionHappiness Unstake" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
